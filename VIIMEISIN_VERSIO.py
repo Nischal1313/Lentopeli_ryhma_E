1e7237051417c2a7eec447748b9752ad3f22940b
Hetahar <heta.hara@gmail.com>
2023-10-12 13:27:00 +0300

    toimii lol

--- VIIMEISIN_VERSIO.py
+++ VIIMEISIN_VERSIO.py
@@ -360,6 +360,13 @@ def compare_save(crime_stopped4, km3, coin4, user_name): # korjaa silleen että 
                 execute_command(sql)
     return
 
+def game_instructions():
+    print("Hetacoins: \nPelin alussa pelaajalla on 4 kolikkoa. Lentäminen maksaa 1 kolikon, ja lisävihje maksaa 1 kolikon. \nJos lennät oikeaan kohteeseen ilman lisävihjettä, saat 2 kolikkoa lisää. Jos lennät oikeaan kohteeseen \nlisävihjeen kanssa, saat 1 kolikon. Jos kolikot loppuu, häviät pelin.\n")
+    print("Kilometrit: \nLennetyt kilometrit + mahdolliset kilometri rangaistukset. Kilometri rangaistuksen saa, \njos lentää väärään kohteeseen. Rangaistus on matkan pituudesta riippuen (lennetty matka - oikea matka) * 2 \ntai (oikeamatka - lennetty matka) * 2, ja tämä lisätään kilometrimäärään.\n")
+    print("Lentäminen: \nLennät kohteeseen valitsemalla valtion, johon saatu vihje viittaa. \nSaat ilmoituksen mille lentokentälle lensit, ja pysäytettyjen rikosten määrä kasvaa \njos olit ajoissa pysäyttämäsää rikoksen, eli lensit oikein.\n")
+    print("Voittaminen: \nVoitat pelin jos pysäytit vähintään 3 rikosta, eli lensit oikein kolmesti, \nsekä päädyit oikealle lentokentälle. Jos kilometrisi ylittävät oikein lennetyt kilometrit 30%, \ntuhlasit kilometrejä ja et lentänyt ympäristöystävällisesti, eli häviät pelin. \nJos kolikkosi ovat 10 tai yli, lensit taloudellisesti vastuullisesti, ja saat tästä ekstra maininnan!")
+    return
+
 def warning(coins):
     if coins == 0:
         print("")
